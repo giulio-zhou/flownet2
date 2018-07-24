c7c137f937a333e6bf36356b0d4e30d14e691886
Giulio Zhou <giuliozhou8@gmail.com>
2018-07-24 21:05:07 +0000

    Modify scripts to use hdf5 files

--- scripts/run-flownet-video.py
+++ scripts/run-flownet-video.py
@@ -8,12 +8,14 @@ from scipy import misc
 import caffe
 import tempfile
 from math import ceil
+import h5py
 import skvideo.io
 
 parser = argparse.ArgumentParser()
 parser.add_argument('caffemodel', help='path to model')
 parser.add_argument('deployproto', help='path to deploy prototxt template')
 parser.add_argument('videopath', help='video on which to run flownet')
+parser.add_argument('batchsize', help='number of images to use in a batch', default=1, type=int)
 parser.add_argument('num_frames', help='number of video frames', default=100, type=int)
 parser.add_argument('outputpath', help='path to numpy array output')
 parser.add_argument('--gpu',  help='gpu id to use (0, 1, ...)', default=0, type=int)
@@ -42,6 +44,8 @@ vars['ADAPTED_HEIGHT'] = int(ceil(height/divisor) * divisor)
 vars['SCALE_WIDTH'] = width / float(vars['ADAPTED_WIDTH']);
 vars['SCALE_HEIGHT'] = height / float(vars['ADAPTED_HEIGHT']);
 
+vars['BATCH_SIZE'] = args.batchsize
+
 tmp = tempfile.NamedTemporaryFile(mode='w', delete=False)
 
 proto = open(args.deployproto).readlines()
@@ -61,37 +65,21 @@ caffe.set_device(args.gpu)
 caffe.set_mode_gpu()
 net = caffe.Net(tmp.name, args.caffemodel, caffe.TEST)
 
-img1, img2 = None, next(vreader) 
+imgs = [next(vreader)]
 output_flow = []
-for j in range(args.num_frames):
-    img1, img2 = img2, next(vreader) 
+hdf5_file = h5py.File(args.outputpath, 'w')
+hdf5_file.create_dataset('data', [args.num_frames - 1, height, width, 2], 'f4')
+for j in range(0, args.num_frames - 1, args.batchsize):
+    start, end = j, min(j + args.batchsize, args.num_frames - 1)
+    imgs = [imgs[-1]] + [next(vreader) for _ in range(start, end)]
+    if len(imgs) < args.batchsize + 1:
+        imgs = imgs + [imgs[-1]] * (args.batchsize + 1 - len(imgs))
     input_dict = {}
-    input_dict[net.inputs[0]] = np.transpose(np.expand_dims(img1, 0), (0, 3, 1, 2))
-    input_dict[net.inputs[1]] = np.transpose(np.expand_dims(img2, 0), (0, 3, 1, 2))
-    #
-    # There is some non-deterministic nan-bug in caffe
-    #
-    i = 1
-    while i<=5:
-        i+=1
-    
-        net.forward(**input_dict)
-    
-        containsNaN = False
-        for name in net.blobs:
-            blob = net.blobs[name]
-            has_nan = np.isnan(blob.data[...]).any()
-    
-            if has_nan:
-                print('blob %s contains nan' % name)
-                containsNaN = True
-    
-        if not containsNaN:
-            print('%d: Succeeded.' % j)
-            break
-        else:
-            print('**************** FOUND NANs, RETRYING ****************')
+    input_dict[net.inputs[0]] = np.transpose(imgs[:-1], (0, 3, 1, 2))
+    input_dict[net.inputs[1]] = np.transpose(imgs[1:], (0, 3, 1, 2))
+    net.forward(**input_dict)
 
-    blob = np.squeeze(net.blobs['predict_flow_final'].data).transpose(1, 2, 0)
-    output_flow.append(np.copy(blob))
-np.save(args.outputpath, np.stack(output_flow, axis=0))
+    blob = net.blobs['predict_flow_final'].data.transpose(0, 2, 3, 1)
+    blob = blob[:end-start]
+    hdf5_file['data'][j:j+end-start] = np.copy(blob)
+    print(j, args.num_frames)
